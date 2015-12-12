6c2f8fead88cb48de2a53dc754b82430405e946d
leyarotheconquerer <HazenJohnson@letu.edu>
2015-12-12 15:43:23 -0600

    Add basic growth.
    Unfortunately, the tilemap is not updating correctly.
    
    I'm going to try a custom sprite solution.

--- src/gamemodes/game.js
+++ src/gamemodes/game.js
@@ -5,6 +5,7 @@ OverGrown.Game = function() {
 	this.plantLayer;
 	this.playerLayer;
 	this.tiles;
+
 	this.tickTimer;
 
 	this.player = {
@@ -12,10 +13,31 @@ OverGrown.Game = function() {
 		y: 0,
 		targetX: 0,
 		targetY: 0,
-		influence: 3,
+		influence: 1,
 		sprite: null
 	};
-	this.nearTiles = [];
+
+	this.enemy = {
+		x: 0,
+		y: 0,
+		targetX: 0,
+		targetY: 0,
+		influence: 1
+	};
+
+	this.ownershipVal = 5;
+	this.growthRate = {
+		grass: {
+			growth: 1,
+			expansion: 1,
+			strength: 1
+		},
+		weed: {
+			growth: 1,
+			expansion: 1,
+			strength: 1
+		}
+	}
 };
 
 // Define the member functions of the game state
@@ -38,13 +60,13 @@ OverGrown.Game.prototype = {
 
 	// Creates objects for this state
 	create: function() {
-		// Construct the level
-		this.constructLevel();
-
 		// Set up input listeners
 		this.game.input.addMoveCallback(this.updateTarget, this);
 		this.player.sprite = this.game.add.sprite();
 		this.game.camera.follow(this.player.sprite, Phaser.Camera.FOLLOW_TOPDOWN);
+
+		// Construct the level
+		this.constructLevel();
 
 		// Set up the tick function
 		this.tickTimer = this.game.time.create(false);
@@ -65,10 +87,6 @@ OverGrown.Game.prototype = {
 		this.plantLayer = this.tilemap.createBlankLayer('plant', 40, 30, 32, 32);
 		this.playerLayer = this.tilemap.createBlankLayer('player', 40, 30, 32, 32);
 
-		// Random player starting point
-		this.player.x = this.game.rnd.integerInRange(0, this.tilemap.width);
-		this.player.y = this.game.rnd.integerInRange(0, this.tilemap.height);
-
 		// Initialize the ground layer
 		for(var i = 0; i < this.tilemap.width; ++i) {
 			for(var j = 0; j < this.tilemap.height; ++j) {
@@ -82,10 +100,19 @@ OverGrown.Game.prototype = {
 					neutral: 5,
 					grass: 0,
 					weed: 0,
-					cattail: 0
+					cattail: 0,
+					current: "neutral"
 				};
 			}
 		}
+
+		// Random player starting point
+		this.player.x = this.game.rnd.integerInRange(0, this.tilemap.width);
+		this.player.y = this.game.rnd.integerInRange(0, this.tilemap.height);
+		this.tilemap.putTile(this.tiles.grass, this.player.x, this.player.y, this.plantLayer);
+		var tile = this.tilemap.getTile(this.player.x, this.player.y, this.playerLayer, true);
+		this.player.sprite.x = tile.worldX;
+		this.player.sprite.y = tile.worldY;
 	},
 
 	// Frame update function
@@ -96,9 +123,8 @@ OverGrown.Game.prototype = {
 	tick: function() {
 		console.log("tick")
 		this.updateDedicated();
-
-		// Calculate the near tiles
-		this.nearTiles = this.getNearTiles(this.player.x, this.player.y, this.player.influence);
+		this.updateTiles();
+		console.log("finished tick");
 	},
 
 	// Updates the target for the dedicated tile
@@ -127,7 +153,7 @@ OverGrown.Game.prototype = {
 				nexty = this.player.y - 1;
 			}
 
-			if(this.tilemap.getTile(nextx, nexty, this.groundLayer, true).index == this.tiles.ground) {
+			if(this.tilemap.getTile(nextx, nexty, this.plantLayer, true).index == this.tiles.grass) {
 				this.tilemap.putTile(this.tiles.empty, this.player.x, this.player.y, this.playerLayer);
 				this.player.x = nextx;
 				this.player.y = nexty;
@@ -138,13 +164,88 @@ OverGrown.Game.prototype = {
 		}
 	},
 
-	// Finds the group of near tiles
+	// Updates the properties of all tiles
+	updateTiles: function() {
+		var playerNearTiles = this.getNearTiles(this.player.x, this.player.y, this.player.influence);
+		var enemyNearTiles = this.getNearTiles(this.enemy.x, this.enemy.y, this.enemy.influence);
+		var updatedTiles = [];
+
+		// Calculate conviction for all "near" tiles to the player
+		for(var i = 0; i < playerNearTiles.length; ++i) {
+			var tile = playerNearTiles[i];
+			if(tile.conviction.current == 'neutral') {
+				tile.conviction.neutral -= Math.floor(Math.sqrt(this.growthRate.grass.expansion));
+				tile.conviction.grass += Math.floor(Math.sqrt(this.growthRate.grass.expansion));
+				updatedTiles.push(tile);
+			} else if (tile.conviction.current == 'weed' && this.growthRate.grass.strength > this.growthRate.weed.strength) {
+				tile.conviction.weed -= Math.floor(Math.sqrt(this.growthRate.grass.strength - this.growthRate.weed.strength));
+				tile.conviction.grass += Math.floor(Math.sqrt(this.growthRate.grass.strength - this.growthRate.weed.strength));
+				updatedTiles.push(tile);
+			}
+		}
+
+		// Calculate conviction for all "near" tiles to the enemy
+		for(var i = 0; i < enemyNearTiles.length; ++i) {
+			var tile = enemyNearTiles[i];
+			if(tile.conviction.current == 'neutral') {
+				tile.conviction.neutral -= Math.floor(Math.sqrt(this.growthRate.weed.expansion));
+				tile.conviction.weed += Math.floor(Math.sqrt(this.growthRate.weed.expansion));
+				updatedTiles.push(tile);
+			} else if (tile.conviction.current == 'weed' && this.growthRate.weed.strength > this.growthRate.grass.strength) {
+				tile.conviction.grass -= Math.floor(Math.sqrt(this.growthRate.weed.strength - this.growthRate.grass.strength));
+				tile.conviction.weed += Math.floor(Math.sqrt(this.growthRate.weed.strength - this.growthRate.grass.strength));
+				updatedTiles.push(tile);
+			}
+		}
+
+		// Determine ownership of tile based on conviction
+		for(var i = 0; i < updatedTiles.length; ++i) {
+			var tile = updatedTiles[i];
+			// Ignore water
+			if(this.tilemap.getTile(tile.x, tile.y, this.groundLayer).index == this.tiles.water) {
+				continue;
+			}
+			// If neutral tile is convinced by someone
+			if(tile.conviction.current == 'neutral' && tile.conviction.neutral <= 0) {
+				// Assign it to the first player to get ownership
+				if(tile.conviction.grass >= this.ownershipVal) {
+					tile.conviction.current = 'grass';
+					tile.conviction.grass = this.ownershipVal;
+					tile.conviction.weed = 0;
+					tile.index = this.tiles.grass;
+				} else if(tile.conviction.weed >= this.ownershipVal) {
+					tile.conviction.weed = 'weed';
+					tile.conviction.grass = 0;
+					tile.conviction.weed = this.ownershipVal;
+					tile.index = this.tiles.weed;
+				}
+				tile.conviction.neutral = 0;
+			// If the grass tile is reduced to 0 conviction
+			} else if (tile.conviction.current == 'grass' && tile.conviction.grass <= 0) {
+				tile.conviction.current = 'weed';
+				tile.conviction.grass = 0;
+				tile.conviction.weed = this.ownershipVal;
+				tile.conviction.neutral = 0;
+				tile.index = this.tiles.weed;
+			// If the weed tile is reduced to 0 conviction
+			} else if (tile.conviction.current == 'weed' && tile.conviction.weed <= 0) {
+				tile.conviction.current = 'grass';
+				tile.conviction.grass = this.ownershipVal;
+				tile.conviction.weed = 0;
+				tile.conviction.neutral = 0;
+				tile.index = this.tiles.grass;
+			}
+		}
+	},
+
+	// Recalculates the group of near tiles
 	getNearTiles: function(x, y, rad) {
 		// Determine a region of feasible "near" tiles
 		var xmin = Math.max(0, Math.min(x - rad, this.tilemap.width));
 		var xmax = Math.max(0, Math.min(x + rad, this.tilemap.width));
 		var ymin = Math.max(0, Math.min(y - rad, this.tilemap.width));
 		var ymax = Math.max(0, Math.min(y + rad, this.tilemap.width));
+
 		var nearTiles = [];
 
 		// Iterate through all possible tiles and determine which tiles are in the radius
