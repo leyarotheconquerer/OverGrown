bda448817d16d9859a5c5d2a97be6b9f4d834c26
leyarotheconquerer <HazenJohnson@letu.edu>
2015-12-12 09:00:43 -0600

    Load the game from the menu
    Add some placeholder tiles

--- src/gamemodes/game.js
+++ src/gamemodes/game.js
@@ -1,18 +1,42 @@
 // Declare the game state
 OverGrown.Game = function() {
-
+	var tilemap;
+	var groundLayer;
+	var plantLayer;
 };
 
 // Define the member functions of the game state
 OverGrown.Game.prototype = {
 	// Loads system resources
 	preload: function() {
-
+		this.game.load.image('tiles', 'assets/Tiles.png');
 	},
 
 	// Creates objects for this state
 	create: function() {
+		this.constructLevel();
+	},
 
+	// Constructs the level
+	constructLevel: function() {
+		// Create an emtpy tile map
+		tilemap = this.game.add.tilemap();
+		tilemap.addTilesetImage('tiles');
+
+		// Add the ground layer
+		this.groundLayer = tilemap.create('ground', 40, 30, 32, 32);
+		this.plantLayer = tilemap.createBlankLayer('plant', 40, 30, 32, 32);
+
+		// Initialize the ground layer
+		for(var i = 0; i < tilemap.width; ++i) {
+			for(var j = 0; j < tilemap.height; ++j) {
+				if((i + j) % 2 == 0) {
+					tilemap.putTile(0, i, j, this.groundLayer);
+				} else {
+					tilemap.putTile(3, i, j, this.groundLayer);
+				}
+			}
+		}
 	},
 
 	// Frame update function
--- src/gamemodes/menu.js
+++ src/gamemodes/menu.js
@@ -13,10 +13,18 @@ OverGrown.Menu.prototype = {
 	// Creates objects for this state
 	create: function() {
 		this.game.add.sprite(0,0,"title");
+		var text = this.game.add.text(0, 0, "Press Spacebar to Begin", {
+			font: 'bold 20pt Arial',
+			boundsAlignH: 'center',
+			boundsAlignV: 'middle'
+		});
+		text.setTextBounds(0, 475, 800, 100);
 	},
 
 	// Frame update function
 	update: function() {
-
+		if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+			this.state.start('Game');
+		}
 	}
 };
